ed4975e0ca4c9b435a615604d70f12e23b0187fe
PatrikRasch <patrikrask@hotmail.com>
2022-09-17 10:30:31 +0200

    Small altercations to the restaurant homepage, nothing major.

---
+++ script.js
@@ -0,0 +1,29 @@
+const pageLoad = () => {
+  <div class="wrapper">
+    <div class="header">
+      <img src="../img/logo.png" alt="" class="imgLogo" />
+      <div class="options">
+        <div class="option1">Breakfast</div>
+        <div class="option1">Lunch</div>
+        <div class="option1">Dinner</div>
+      </div>
+    </div>
+
+    <div class="mainContent">
+      <div class="leftSide">
+        <div class="title">Piada Italian Street Food</div>
+        <div class="description">
+          Piada Italian Street Food is great it might obliterate your insides.
+          <br />
+          <i>
+            Matter of fact, only 15% of our customers leave without spending 30
+            minutes at our bathroom prior to departure.
+          </i>
+        </div>
+        <button class="order">Make an order</button>
+      </div>
+      <img src="../img/burger.png" alt="" class="imgBurger" />
+    </div>
+    <div class="footer"></div>
+  </div>;
+};
